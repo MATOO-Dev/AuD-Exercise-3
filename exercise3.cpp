6584915bb2b4bf8411fb67dd8f95b3060ba0a52b
MATOO-Dev <fagoldob@gmail.com>
2023-04-04 23:17:32 +0200

    Fixed pi algorithm orders of operations mistake

--- exercise3.cpp
+++ exercise3.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <iostream>
 #include <ostream>
 
@@ -23,35 +24,26 @@ float hn_i(int n)
     return result;
 }
 
-double piRecursive(int remainingDepth)
+//take result * 2 when calling initially
+double piRecursive(int steps, int maxSteps)
 {
-    if(remainingDepth == 0)
-    return 1;
+    if(steps == maxSteps)
+        return 1;
 
-    double division = (double)remainingDepth / double(remainingDepth * 2 + 1);
-    double currentStep = 1 + division;
-    std::cout << "current step for depth " << remainingDepth << " is " << currentStep << std::endl;
-    return currentStep * piRecursive(remainingDepth - 1);
-}
-
-double piRecursiveV2(int remainingDepth)
-{
-    if(remainingDepth == 0)
-    return 1;
-
-    double division = (double)remainingDepth / double(remainingDepth * 2 + 1);
-    double currentStep = division;
-    std::cout << "current step for depth " << remainingDepth << " is " << currentStep << std::endl;
-    return currentStep * piRecursive(remainingDepth - 1);
+    double division = (double)steps / double(steps * 2 + 1);
+    double bracketMult = division * piRecursive(steps + 1, maxSteps);
+    return 1 + bracketMult;
 }
 
 int main()
 {
     //8
     //depth 5 should return ~2.28
+    std::cout << "starting exercise 8)" << std::endl << "a)" << std::endl;
     //a
     std::cout << hn_r(5) << std::endl;
     //b
+    std::cout << "b) " << std::endl;
     std::cout << hn_i(5) << std::endl;
 
     //9
@@ -62,25 +54,11 @@ int main()
     //zum bruch ergebnis dann 1 dazu zählen
 
     //b
-    double pi2 = 2 * piRecursive(9);
-    std::cout << "Die ersten Stellen von Pi: " << pi2 << std::endl;
-
-    //just a test
-    double step1 = double(1)/double(3) + 1;
-    double step2 = double(2)/double(5) + 1;
-    double step3 = double(3)/double(7) + 1;
-    double step4 = double(4)/double(9) + 1;
-    double step5 = double(5)/double(11) + 1;
-    double step6 = double(6)/double(13) + 1;
-    double step7 = double(7)/double(15) + 1;
-    double step8 = double(8)/double(17) + 1;
-    double step9 = double(9)/double(19) + 1;
-
-    double test = 2 * step1 * step2 * step3 * step4 * step5 * step6 * step7 * step8 * step9;
-
-    std::cout << "result is " << test << std::endl;
+    double pi = 2 * piRecursive(1, 10000);
+    std::cout << "starting exercise 9)" << std::endl;
+    printf("Die ersten Stellen von Pi: %.10lf", pi);
+    std::cout << std::endl;
     return 0;
-
 
     //10 -> see paper
     //11 -> see paper
